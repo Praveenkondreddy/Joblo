b3071b829517287ac55108c4d7db7f4ac677be5a
Praveen Kondreddy <praveennov26@gmail.com>
2022-06-15 18:17:53 +0530

    first commit

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -16,56 +16,57 @@ function Profile() {
 } 
 
   return (
-    <div class="container">
+    <div class="profile-container">
      
     <div class="form-class">
+    <div class="profile-inner">
     <form onSubmit={submitHandler}>
-     <div class="profile-inner">
+     
       <h4>Please edit your details</h4>
       
       <div class="form-group">
       <label htmlFor='name'>Name : </label>
       <input type="text" class="input" name="name" onChange={e =>setDetails({...details, name:e.target.value})} value={details.name}/>  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <label htmlFor='email'>Email  Id : </label>
       <input type="text" class="input" name="email" onChange={e =>setDetails({...details, email:e.target.value})} value={details.email}/>  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <label htmlFor='phone'>Phone No : </label>
       <input type="text" class="input" name="phone" onChange={e =>setDetails({...details, phone:e.target.value})} value={details.phone} />  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <label htmlFor='interMarks'>Inter Marks : </label>
       <input type="text" class="input" name="interMarks" onChange={e =>setDetails({...details, interMarks:e.target.value})} value={details.interMarks} />  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <label htmlFor='degreePercentage'>Degree Percentage : </label>
       <input type="text" class="input" name="degreePercentage" onChange={e =>setDetails({...details, degreePercentage:e.target.value})} value={details.degreePercentage} />  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <label htmlFor='currentOrganisation'>Current Organisation: </label>
       <input type="text" class="input" name="currentOrganisation" onChange={e =>setDetails({...details, currentOrganisation:e.target.value})} value={details.currentOrganisation} />  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <label htmlFor='experience'>Experience : </label>
       <input type="text" class="input" name="experience" onChange={e =>setDetails({...details, experience:e.target.value})} value={details.experience} />  
       </div>
-      <hr />
+      <br></br>
       <div class="form-group">
       <input type="submit" class="login-input" value="Edit profile" />  
-      </div><br />
+      </div> <br></br>
      
   
-     </div>
+     
     </form>
-    
+    </div>
     </div>
     
     </div>
